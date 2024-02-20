4071d10b761a48fea3909bff06ee2356002d33b7
Peng Likim <penglikimpenglikim6666@gmail.com>
2024-02-20 11:04:17 +0700

    check routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import PathConstants from "./pathConstants"
+import Home from "../pages/Home"
+import Team from "../pages/Team"
+import Portfolio from "../pages/Portfolio"
+import About from "../pages/About"
 
-const Home = React.lazy(() => import("../pages/Home"))
-const Team = React.lazy(() => import("../pages/Team"))
-const Portfolio = React.lazy(() => import("../pages/Portfolio"))
-const About = React.lazy(() => import("../pages/About"))
+// const Home = React.lazy(() => import("../pages/Home"))
+// const Team = React.lazy(() => import("../pages/Team"))
+// const Portfolio = React.lazy(() => import("../pages/Portfolio"))
+// const About = React.lazy(() => import("../pages/About"))
 
 const routes = [
     { path: PathConstants.HOME, element: <Home /> },

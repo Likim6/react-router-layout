8feb5d9d57121cba3ffbcf8389396320fdcea8b5
Peng Likim <penglikimpenglikim6666@gmail.com>
2024-02-20 11:02:42 +0700

    data added

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,7 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
 export default function Team() {
-    return (
-        <div>
-            <h1>Team page</h1>
-        </div>
-    )
+    const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const jsonData = await response.json();
+        setData(jsonData);
+        console.log(jsonData);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Description</th>
+              <th>Category</th>
+              <th>Rating</th>
+              <th>Image</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.title}</td>
+                <td>{item.price} $</td>
+                <td>{item.description}</td>
+                <td>{item.category}</td>
+                <td>{item.rating.rate} / {item.rating.count}</td>
+                <td><img src={item.image} alt={item.title} /></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
